bf6df3519c3dd7b24ddae1279206b708a014a303
Collier-A-B <benjaminac46@gmail.com>
2025-07-17 13:03:48 -0400

    bean factory tutorial complete

--- shopping_system/src/main/java/collier/shopping_system/geeks_for_geeks_demo/bean_factory/BeanFactoryApp.java
+++ shopping_system/src/main/java/collier/shopping_system/geeks_for_geeks_demo/bean_factory/BeanFactoryApp.java
@@ -1,5 +1,18 @@
 package collier.shopping_system.geeks_for_geeks_demo.bean_factory;
 
-public class BeanFactoryApp {
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@SpringBootApplication
+public class BeanFactoryApp {
+// Main driver method
+  public static void main(String[] args) 
+  {
+    // Creating object in a spring container (Beans)
+    BeanFactory factory = new ClassPathXmlApplicationContext("bean-factory-demo.xml");
+    Student student = (Student) factory.getBean("student");
+
+    System.out.println(student);
+  }
 }
